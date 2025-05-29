6e9b7c08397be9d0aca4a9203e3980e8208bbcde
Nawroj <82165840+Nawroj@users.noreply.github.com>
2025-05-29 20:18:13 +1000

    fixed the login issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { jwtDecode } from 'jwt-decode';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import IPThreats from './components/IPThreats';
@@ -11,22 +12,29 @@ import RegkeyThreats from './components/RegkeyThreats';
 import Register from './components/Register';
 import AttributeDetail from './components/AttributeDetail';
 
+
 function App() {
   const [user, setUser] = useState(null);
   const [checkingAuth, setCheckingAuth] = useState(true);
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      setUser({ token });
+  const token = localStorage.getItem('token');
+  if (token) {
+    try {
+      const decoded = jwtDecode(token);
+      const currentTime = Date.now() / 1000;
+      if (decoded.exp > currentTime) {
+        setUser({ token });
+      } else {
+        localStorage.removeItem('token'); // Token expired
+      }
+    } catch (error) {
+      console.error("Invalid token");
+      localStorage.removeItem('token');
     }
-    setCheckingAuth(false);
-  }, []);
-
-  if (checkingAuth) {
-    // Show a loading indicator or blank screen until auth status is checked
-    return;
   }
+  setCheckingAuth(false);
+}, []);
 
   return (
     <Router>
